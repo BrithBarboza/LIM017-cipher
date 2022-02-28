8673efe9ad162771b24eb876f67e7acf225d3088
BrithBarboza <brith.19.04@gmail.com>
2022-02-28 09:32:57 -0500

    project final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+window.addEventListener("load", inicio, true);
+
+//Mayúsculas
+function inicio() {
+    document.getElementById("text").addEventListener("keyup", function () {
+        this.value = this.value.toUpperCase(); //con this llamo al elemento que previamente he llamado (text)
+    }, true);
+
+    document.getElementById("btn1").addEventListener("click", function () {
+        let string = document.getElementById("text").value;
+        let offset = document.getElementById("offset").value;
+        document.getElementById("text2").value = cipher.encode(offset, string);
+    }, true);
+
+    document.getElementById("btn2").addEventListener("click", function () {
+        let string = document.getElementById("text").value;
+        let offset = document.getElementById("offset").value;
+        document.getElementById("text2").value = cipher.decode(offset, string);
+    }, true);
+
+}
+
+
+console.log('cipher');
